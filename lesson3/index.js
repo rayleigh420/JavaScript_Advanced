a2e28e138cf20b78a76f5d514a6678f363b1f257
rayleigh420 <nhatduy0409@gmail.com>
2023-02-22 21:01:47 +0700

    lesson3 - Kind of function

---
+++ lesson3/index.js
@@ -0,0 +1,16 @@
+// Kind of function
+
+// Declaration Function
+function show() {
+    console.log("Hello")
+}
+// Expression Function
+// Co the dat ten hoac khong
+var show1 = function () {
+    console.log("World")
+}
+setTimeout(function () {
+    console.log("Time Out")
+})
+
+// Arrow Function
